9529efaa7dbc73bc1916d0428f809400a8ceb12e
apple-yagi <applesryu@gmail.com>
2022-07-10 20:38:20 +0900

    Add go-chi/chi and rs/cors

--- server/server.go
+++ server/server.go
@@ -9,11 +9,21 @@ import (
 	"github.com/99designs/gqlgen/graphql/playground"
 	"github.com/apple-yagi/next-golang-gql/server/graph"
 	"github.com/apple-yagi/next-golang-gql/server/graph/generated"
+	"github.com/go-chi/chi"
+	"github.com/rs/cors"
 )
 
 const defaultPort = "8080"
 
 func main() {
+	router := chi.NewRouter()
+
+	router.Use(cors.New(cors.Options{
+		AllowedOrigins:   []string{"http://localhost:3000"},
+		AllowCredentials: true,
+		Debug:            true,
+	}).Handler)
+
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = defaultPort
@@ -21,8 +31,13 @@ func main() {
 
 	srv := handler.NewDefaultServer(generated.NewExecutableSchema(generated.Config{Resolvers: &graph.Resolver{}}))
 
-	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
-	http.Handle("/query", srv)
+	router.Handle("/", playground.Handler("GraphQL playground", "/query"))
+	router.Handle("/query", srv)
+
+	err := http.ListenAndServe(":"+port, router)
+	if err != nil {
+		panic(err)
+	}
 
 	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
 	log.Fatal(http.ListenAndServe(":"+port, nil))
